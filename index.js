c38c5eb1a47393501b522f782c62dc627967e833
shiner0905 <shiner0905@gmail.com>
2024-10-31 23:31:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -408,140 +408,350 @@ function createButton(profile, body) {
                 "type": "flex",
                 "altText": "this is a flex message",
                 "contents": {
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://developers-resource.landpress.line.me/fx/img/01_1_cafe.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover",
-    "action": {
-      "type": "uri",
-      "uri": "https://line.me/ti/p/ysF_vZidmC"
-    },
-    "margin": "none"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "楊依依",
-        "weight": "bold",
-        "size": "xl"
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/6wFlzgT.jpeg",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "uri": "https://line.me/ti/p/ysF_vZidmC"
+        },
+        "margin": "none"
       },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "margin": "md",
-        "contents": [
-          {
-            "type": "icon",
-            "size": "xxl",
-            "url": "https://i.imgur.com/htPs7fR.png",
-            "margin": "none"
-          },
-          {
-            "type": "text",
-            "text": "Lamb5411",
-            "size": "xxl",
-            "color": "#999999",
-            "margin": "md",
-            "flex": 0
-          }
-        ]
-      },
-      {
+      "body": {
         "type": "box",
         "layout": "vertical",
-        "margin": "lg",
-        "spacing": "sm",
         "contents": [
           {
-            "type": "separator"
+            "type": "text",
+            "text": "楊依依",
+            "weight": "bold",
+            "size": "xl"
           },
           {
             "type": "box",
             "layout": "baseline",
-            "spacing": "md",
+            "margin": "md",
             "contents": [
               {
+                "type": "icon",
+                "size": "xxl",
+                "url": "https://i.imgur.com/htPs7fR.png",
+                "margin": "none"
+              },
+              {
                 "type": "text",
-                "text": "永慶不動產北屯823公園店",
-                "wrap": true,
-                "color": "#666666",
-                "size": "xl",
-                "flex": 5
+                "text": "Lamb5411",
+                "size": "xxl",
+                "color": "#999999",
+                "margin": "md",
+                "flex": 0
               }
             ]
           },
           {
             "type": "box",
-            "layout": "baseline",
+            "layout": "vertical",
+            "margin": "lg",
             "spacing": "sm",
             "contents": [
               {
-                "type": "text",
-                "text": "手機",
-                "size": "lg",
-                "flex": 1,
-                "color": "#FF0000"
+                "type": "separator"
               },
               {
-                "type": "text",
-                "text": "0973-005-411",
-                "wrap": true,
-                "color": "#FF0000",
-                "size": "xl",
-                "flex": 5
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "md",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "永慶不動產北屯823公園店",
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "xl",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "手機",
+                    "size": "lg",
+                    "flex": 1,
+                    "color": "#FF0000"
+                  },
+                  {
+                    "type": "text",
+                    "text": "0973-005-411",
+                    "wrap": true,
+                    "color": "#FF0000",
+                    "size": "xl",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "電話",
+                    "flex": 1,
+                    "size": "lg"
+                  },
+                  {
+                    "type": "text",
+                    "text": "04-2422-6499",
+                    "flex": 5,
+                    "size": "xl",
+                    "wrap": true
+                  }
+                ],
+                "spacing": "sm"
               }
-            ]
+            ],
+            "borderWidth": "none"
           },
           {
             "type": "box",
-            "layout": "baseline",
+            "layout": "vertical",
             "contents": [
               {
-                "type": "text",
-                "text": "電話",
-                "flex": 1,
-                "size": "lg"
+                "type": "separator"
               },
               {
                 "type": "text",
-                "text": "04-2422-6499",
-                "flex": 5,
-                "size": "xl",
-                "wrap": true
+                "text": "台中市北屯區崇德十路一段390號",
+                "size": "md"
               }
-            ],
-            "spacing": "sm"
+            ]
           }
-        ],
-        "borderWidth": "none"
+        ]
       },
-      {
+      "styles": {
+        "header": {
+          "separator": true
+        }
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
         "type": "box",
         "layout": "vertical",
         "contents": [
           {
-            "type": "separator"
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
           },
           {
-            "type": "text",
-            "text": "台中市北屯區崇德十路一段390號",
-            "size": "md"
+            "type": "image",
+            "url": "https://i.imgur.com/t9Spacv.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
           }
         ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
       }
-    ]
-  },
-  "styles": {
-    "header": {
-      "separator": true
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
+          },
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/YtHG4wn.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
+          },
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/0tWFHwH.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
+          },
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/C0Aadgr.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
+          },
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/maTtNNi.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/NHlYXbW.png",
+            "position": "relative",
+            "size": "md",
+            "offsetTop": "0px",
+            "offsetStart": "none",
+            "margin": "none"
+          },
+          {
+            "type": "image",
+            "url": "https://i.imgur.com/2O1T41t.png",
+            "size": "full",
+            "action": {
+              "type": "uri",
+              "label": "action",
+              "uri": "https://line.me/ti/p/ysF_vZidmC"
+            }
+          }
+        ],
+        "flex": 0
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [],
+        "flex": 0
+      }
     }
-  }
+  ]
 }
               }
         ])
