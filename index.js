588409d5790a2bf757fe751b4f5e3ae07049eef9
shiner0905 <shiner0905@gmail.com>
2024-10-31 22:28:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -354,10 +354,35 @@ function createButton(profile, body) {
           //                 ]
           //               }
           // }
-          {
-              "type": "text",
-              "text": "Hello, world"
-          }
+          // {
+          //     "type": "text",
+          //     "text": "Hello, world"
+          // }
+            {
+              "type": "template",
+              "altText": "This is a buttons template",
+              "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "Menu",
+                "text": "Please select",
+                "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/123"
+                },
+                "actions": [
+                  {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
+                  }
+                ]
+              }
+            }
         ])
 
         if (result) {
