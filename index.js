708c709c844e5aa638b2570ba3bf396417a5c92d
shiner0905 <shiner0905@gmail.com>
2024-10-31 21:57:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,44 @@ async function liffInit() {
   }
 }
 
-async function liffProfile() {
-  const profile = await liff.getProfile();
-  const displayName = profile.displayName;
-  const h1 = document.createElement("h1");
-  const textNode = document.createTextNode(`Hello ${displayName}`);
-  h1.appendChild(textNode);
+function createButton(profile, body) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const userImage = profile.pictureUrl;
+  const userReply = [urlParams.get('name'), urlParams.get('phone'), urlParams.get('email'), userImage];
+  const flexContent = flexCard(userReply);
+  const handleClick = async () => {
+    if (liff.isApiAvailable("shareTargetPicker")) {
+      try {
+        const result = await liff.shareTargetPicker([
+          {
+            "type": "flex",
+            "altText": `${urlParams.get('name')} present name card from Netlify`,
+            "contents": flexContent
+          }
+        ])
 
-  const body = document.querySelector('body');
-  body.appendChild(h1);
+        if (result) {
+          alert('Flex Message success');
+        }
+
+      } catch (error) {
+        alert("Flex Message got some error");
+      }
+    }
+  }
+
+  const button = document.createElement("button");
+  button.innerHTML = "Share Your Name Card";
+  button.onclick = handleClick;
+
+  body.appendChild(button);
 }
 
 async function main() {
   await liffInit()
-  await liffProfile()
+  const profile = await liff.getProfile();
+  const body = document.querySelector('body');
+  createButton(profile, body);
 }
 
 main()
