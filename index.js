4705ab0ad97113a3fad4dbcbceb064d476b8450e
shiner0905 <shiner0905@gmail.com>
2024-11-01 21:03:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,43 +5,43 @@ async function liffInit() {
   }
 }
 
-function createButton(profile, body) {
-  const handleClick = async () => {
-    const textareaContent = document.getElementById("flex_message").value;
-    const flexMessage = JSON.parse(textareaContent);
+// function createButton(profile, body) {
+//   const handleClick = async () => {
+//     const textareaContent = document.getElementById("flex_message").value;
+//     const flexMessage = JSON.parse(textareaContent);
 
-    if (liff.isApiAvailable("shareTargetPicker")) {
-      try {
-        const result = await liff.shareTargetPicker([
-          {
-            "type": "flex",
-            "altText": "[名片]楊依依-永慶不動產北屯823公園店",
-            "contents": flexMessage
-          }
-        ]);
+//     if (liff.isApiAvailable("shareTargetPicker")) {
+//       try {
+//         const result = await liff.shareTargetPicker([
+//           {
+//             "type": "flex",
+//             "altText": "[名片]楊依依-永慶不動產北屯823公園店",
+//             "contents": flexMessage
+//           }
+//         ]);
 
-        if (result) {
-          alert('訊息發送成功');
-        }
+//         if (result) {
+//           alert('訊息發送成功');
+//         }
 
-      } catch (error) {
-        alert("Flex Message got some error");
-      }
-    }
-  }
+//       } catch (error) {
+//         alert("Flex Message got some error");
+//       }
+//     }
+//   }
 
-  const button = document.createElement("button");
-  button.innerHTML = "分享給朋友";
-  button.onclick = handleClick;
+//   const button = document.createElement("button");
+//   button.innerHTML = "分享給朋友";
+//   button.onclick = handleClick;
 
-  body.appendChild(button);
-}
+//   body.appendChild(button);
+// }
 
 async function main() {
   await liffInit();
   const profile = await liff.getProfile();
   const body = document.querySelector('body');
-  createButton(profile, body);
+  // createButton(profile, body);
 }
 
 main();
